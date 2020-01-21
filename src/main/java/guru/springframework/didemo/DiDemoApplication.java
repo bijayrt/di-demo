f15f69885a09dca3388c04a072b80217b07d44ac
bijayrt <bijayrt@gmail.com>
2020-01-21 14:52:17 -0800

    Spring Bean autowired - use of @Autowired @Controller @Service

--- src/main/java/guru/springframework/didemo/DiDemoApplication.java
+++ src/main/java/guru/springframework/didemo/DiDemoApplication.java
@@ -1,9 +1,14 @@
 package guru.springframework.didemo;
 
+import guru.springframework.didemo.controller.ConstructorInjectedController;
 import guru.springframework.didemo.controller.MyController;
+import guru.springframework.didemo.controller.PropertyInjectedController;
+import guru.springframework.didemo.controller.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+
+import java.sql.SQLOutput;
 
 @SpringBootApplication
 public class DiDemoApplication {
@@ -12,6 +17,10 @@ public class DiDemoApplication {
        ApplicationContext ctx =  SpringApplication.run(DiDemoApplication.class, args);
         MyController controller = (MyController) ctx.getBean("myController");
         controller.hello();
+
+        System.out.println("Property    : "+ctx.getBean(PropertyInjectedController.class).sayHello());
+        System.out.println("Setter      : "+ctx.getBean(SetterInjectedController.class).sayHello());
+        System.out.println("Constructor : "+ctx.getBean(ConstructorInjectedController.class).sayHello());
     }
 
 }
--- src/main/java/guru/springframework/didemo/controller/SetterInjectedController.java
+++ src/main/java/guru/springframework/didemo/controller/SetterInjectedController.java
@@ -3,8 +3,12 @@ package guru.springframework.didemo.controller;
 
 import guru.springframework.didemo.services.GreetingService;
 import guru.springframework.didemo.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetterInjectedController {
+    @Autowired
     private GreetingService greetingService;
 
     public String sayHello(){
