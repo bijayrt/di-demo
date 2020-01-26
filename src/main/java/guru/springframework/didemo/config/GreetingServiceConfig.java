a86a0c84007811473e55d75144e67df5fbc14fb7
bijayrt <bijayrt@gmail.com>
2020-01-25 22:09:42 -0800

    Committing Factory Methods. Note: When the Bean configuration such as @Bean, @Primary and @Profile are used in the Class itself, if you use it in the GreetingServiceConfig java, it will give error while loading classes. You can define this in the class itself or when you initialize, but not both!!

--- src/main/java/guru/springframework/didemo/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/didemo/config/GreetingServiceConfig.java
@@ -1,6 +1,7 @@
 package guru.springframework.didemo.config;
 
 import guru.springframework.didemo.services.GreetingRepository;
+import guru.springframework.didemo.services.GreetingService;
 import guru.springframework.didemo.services.GreetingServiceFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,24 +15,15 @@ public class GreetingServiceConfig {
         return new GreetingServiceFactory(repository);
     }
 
-//    @Bean
-//    @Primary
-//    @Profile({"default", "en"})
-//    GreetingService primaryGreetingService(GreetingServiceFactory greetingServiceFactory) {
-//        return greetingServiceFactory.createGreetingService("en");
-//    }
-//
-//    @Bean
-//    @Primary
-//    @Profile("es")
-//    GreetingService primarySpanishGreetingService(GreetingServiceFactory greetingServiceFactory) {
-//        return greetingServiceFactory.createGreetingService("es");
-//    }
-//
-//    @Bean
-//    @Primary
-//    @Profile("de")
-//    GreetingService primaryGermanGreetingService(GreetingServiceFactory greetingServiceFactory) {
-//        return greetingServiceFactory.createGreetingService("de");
-//    }
+    GreetingService primaryGreetingService(GreetingServiceFactory greetingServiceFactory) {
+        return greetingServiceFactory.createGreetingService("en");
+    }
+
+    GreetingService primarySpanishGreetingService(GreetingServiceFactory greetingServiceFactory) {
+        return greetingServiceFactory.createGreetingService("es");
+    }
+
+    GreetingService primaryGermanGreetingService(GreetingServiceFactory greetingServiceFactory) {
+        return greetingServiceFactory.createGreetingService("de");
+    }
 }
